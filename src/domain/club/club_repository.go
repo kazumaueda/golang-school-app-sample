0350ff2a29b4e5265e4c2f5c74dea40b6a36e77e
maloweb3 <ueda1023@gmail.com>
2023-03-23 23:20:34 +0900

    ClubMemberリポジトリを削除

--- src/domain/club/club_repository.go
+++ src/domain/club/club_repository.go
@@ -6,8 +6,5 @@ type ClubRepository interface {
 	Create(ctx context.Context, club *Club) (res *Club, err error)
 	Update(ctx context.Context, club *Club) (res *Club, err error)
 	FindByID(ctx context.Context, clubID string) (res *Club, err error)
-}
-
-type ClubMemberRepository interface {
-	Create(ctx context.Context, clubMember *ClubMember) (res *ClubMember, err error)
+	CreateClubMember(ctx context.Context, clubMember *ClubMember) (res *ClubMember, err error)
 }
